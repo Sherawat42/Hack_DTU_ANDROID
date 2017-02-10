4ecfc71ef8e6944a44a6738c0bb157449e3d19c4
luv-misra <luvmisra97@gmail.com>
2017-02-11 02:55:56 +0530

    login signout history appointmeny

--- app/src/main/java/com/example/luv/med/MainActivity.java
+++ app/src/main/java/com/example/luv/med/MainActivity.java
@@ -8,6 +8,7 @@ import android.app.TaskStackBuilder;
 import android.content.ContentProviderResult;
 import android.content.Context;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -16,54 +17,46 @@ import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.widget.Button;
+import android.widget.EditText;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.Calendar;
 import java.util.Date;
 
+
+
+
+
 public class MainActivity extends AppCompatActivity {
+
+    EditText emailText;
+    EditText passsword;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        emailText = (EditText) findViewById(R.id.emailText);
+        passsword = (EditText) findViewById(R.id.password);
     }
 
     void notify(View v){
         Log.e("1","was here");
-//        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(this);
-//        mBuilder.setSmallIcon(R.drawable.abc);
-//        mBuilder.setContentTitle("Notification Alert, Click Me!");
-//        mBuilder.setContentText("Hi, This is Android Notification Detail!");
-//        Intent resultIntent = new Intent(this, ResultActivity.class);
-//        TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
-//        stackBuilder.addParentStack(ResultActivity.class);
-//        stackBuilder.addNextIntent(resultIntent);
-//       PendingIntent resultPendingIntent = stackBuilder.getPendingIntent(0,PendingIntent.FLAG_UPDATE_CURRENT);
-//        mBuilder.setContentIntent(resultPendingIntent);
-//        NotificationManager mNotificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-//        mNotificationManager.notify(0, mBuilder.build());
-
-        // requestWindowFeature(Window.FEATURE_NO_TITLE);
-//        Date dat  = new Date();//initialises to now
-//        Calendar cal_alarm = Calendar.getInstance();
-//        Calendar cal_now = Calendar.getInstance();
-//        cal_now.setTime(dat);
-//        cal_alarm.setTime(dat);
-//        cal_alarm.set(Calendar.HOUR_OF_DAY,23);//set the alarm time
-//        cal_alarm.set(Calendar.MINUTE, 37);
-//        cal_alarm.set(Calendar.SECOND,0);
-//        if(cal_alarm.before(cal_now)){//if its in the past increment
-//            cal_alarm.add(Calendar.DATE,1);
-//        }
-//        Intent intent = new Intent(this, ResultActivity.class);
-//        PendingIntent AlarmIntent = PendingIntent.getBroadcast(this, 0, intent, 0);
-//        AlarmManager am = (AlarmManager) this.getSystemService(this.ALARM_SERVICE);
-//        am.set(AlarmManager.RTC,cal_alarm.getTimeInMillis(), AlarmIntent);
 
         Intent activate = new Intent(this, NavigationActivity.class);
         AlarmManager alarms ;
         PendingIntent alarmIntent = PendingIntent.getBroadcast(this, 0, activate, 0);
         alarms = (AlarmManager) getSystemService(ALARM_SERVICE);
         alarms.set(AlarmManager.RTC_WAKEUP, System.currentTimeMillis()+5000, alarmIntent);
+    }
+    void login_f(View v){
+//        need to verify data api
+        Intent i = new Intent(this , User_profile.class);
+        finish();
+        startActivity(i);
     }
 }
---
+++ app/src/main/java/com/example/luv/med/User_profile.java
@@ -0,0 +1,32 @@
+package com.example.luv.med;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class User_profile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_profile);
+    }
+    public void appointment(View v )
+    {
+        Intent i = new Intent(this,Appointment.class);
+        startActivity(i);
+    }
+    public void history(View v )
+    {
+        Intent i = new Intent(this,History.class);
+        startActivity(i);
+    }
+    public void signout(View v )
+    {
+        /* user session delete */
+        Intent i = new Intent(this,MainActivity.class);
+        finish();
+        startActivity(i);
+    }
+}
